cc0cadf9127d93b1a275255cf6fd457c853ed5d2
Jurgen Gjonçari <j.gjoncari@albansi.com>
2022-06-02 17:57:53 +0200

    sdfadf

--- script.js
+++ script.js
@@ -2,29 +2,41 @@ const input = document.querySelector('input')
 const submitButton = document.querySelector('[data-submit]')
 
 async function getData(city) {
-    const apiKey = '171cf5d4089515d60f4d9082438dd510'
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+    const weatherApiKey = '171cf5d4089515d60f4d9082438dd510'
+    const giphyApiKey = 'BlVRxPQsFmQQPQQNeQ0kKmEsq366A9GU'
 
-    const response = await fetch(API, { mode: 'cors' })
-    const responseJSON = await response.json()
+    const weatherAPI = `api.openweathermap.org/data/2.5/weather?appid=${weatherApiKey}&q=${city}`
+    const giphySearchAPI = `api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=clouds ${city}`
 
-    return responseJSON.main
-}
+    const weatherResult = await fetch(`http://${weatherAPI}`, { mode: 'cors' })
+    const weatherResponseJSON = await weatherResult.json()
 
+    const giphyResult = await fetch(`http://${giphySearchAPI}`, {mode: 'cors'})
+    const giphyResponseJSON = await giphyResult.json()
 
-const section = document.querySelector('section')
-const list = document.querySelector('dl')
-const dd = list.querySelectorAll('dd')
+    const body = document.querySelector('body')
+    const section = document.querySelector('section')
+    const list = document.querySelector('dl')
+    const dd = list.querySelectorAll('dd')
 
-submitButton.addEventListener('click', async function () {
-    const response = await getData(input.value)
-    for (let [key, value] of Object.entries(response)) {
+    console.log(weatherResponseJSON);
+
+    for (let [key, value] of Object.entries(weatherResponseJSON.main)) {
         for (let d of dd) {
             if (d.id == key) {
                 d.textContent = value
             }
         }
     }
+
     list.style.display = 'initial'
     section.style.display = 'none'
-})
+
+    console.log(giphyResponseJSON);
+
+    body.style.backgroundImage = `url('${giphyResponseJSON.data[0].images.original.url}')`
+    body.style.backgroundRepeat = 'no-repeat'
+    body.style.backgroundSize = 'cover'
+}
+
+submitButton.addEventListener('click', () => {getData(input.value)})
